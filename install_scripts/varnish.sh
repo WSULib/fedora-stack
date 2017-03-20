60d0aa81afe042b9924b89b840af0f9f031465e5
Cole Hudson <colehudson@gmail.com>
2017-03-20 15:08:51 -0400

    Update to xenial
    Removed the references to varnish from a trusty repo. We're now installing from apt-get main repo since that version plays well with systemd.

--- install_scripts/varnish.sh
+++ install_scripts/varnish.sh
@@ -18,8 +18,6 @@ fi
 # install varnish
 echo "apt-get for varnish"
 apt-get -y install apt-transport-https
-curl https://repo.varnish-cache.org/GPG-key.txt | apt-key add -
-echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.1" >> /etc/apt/sources.list.d/varnish-cache.list
 apt-get update
 apt-get -y install varnish
 
