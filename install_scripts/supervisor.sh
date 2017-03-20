d2a5e6df48f7bc0fef7fae949d94d7b2f5859b82
Cole Hudson <colehudson@gmail.com>
2017-03-20 12:59:00 -0400

    16.04 update
    There was a critical bug in supervisor where it didn't restart upon system reboot. A fix has been released; however, it's in the xenial-proposed archive. I'm setting this so that we grab that version of supervisor. Eventually, this will be updated as the package gets into the main xenial release branch.

--- install_scripts/supervisor.sh
+++ install_scripts/supervisor.sh
@@ -15,8 +15,16 @@ else
 fi
 #################################################################
 
+# Update repository listing.
+echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial-proposed restricted main multiverse universe" >> /etc/apt/sources.list
+
+# Let's not let the proposed packages be available except if we ask for them explicitly (like below)
+touch /etc/apt/preferences.d/proposed-updates
+echo $'Package: *\nPin: release a=xenial-proposed\nPin-Priority: 400' >> /etc/apt/preferences.d/proposed-updates
+apt-get -y update
+
 # apt-get install 
-apt-get -y install supervisor
+apt-get -y install supervisor/xenial-proposed
 
 # copy custom supervisor.conf file
 cp $SHARED_DIR/config/ouroboros/supervisord.conf /etc/supervisor/
