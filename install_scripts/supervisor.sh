2d64e88e77640316a9c20c847a263e33eb1a8bf5
Cole Hudson <colehudson@gmail.com>
2017-04-10 08:56:41 -0400

    Removed xenial-proposed supervisor
    supervisor was added to xenial-updates, so no need to get package from -proposed branch

--- install_scripts/supervisor.sh
+++ install_scripts/supervisor.sh
@@ -15,16 +15,8 @@ else
 fi
 #################################################################
 
-# Update repository listing.
-echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial-proposed restricted main multiverse universe" >> /etc/apt/sources.list
-
-# Let's not let the proposed packages be available except if we ask for them explicitly (like below)
-touch /etc/apt/preferences.d/proposed-updates
-echo $'Package: *\nPin: release a=xenial-proposed\nPin-Priority: 400' >> /etc/apt/preferences.d/proposed-updates
-apt-get -y update
-
 # apt-get install 
-apt-get -y install supervisor/xenial-proposed
+apt-get -y install supervisor
 
 # copy custom supervisor.conf file
 cp $SHARED_DIR/config/ouroboros/supervisord.conf /etc/supervisor/
