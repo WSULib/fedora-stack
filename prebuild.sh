f2f68e2cd2a728ecc38832e9d3c05f2ac7218160
Graham Hukill <ghukill@gmail.com>
2017-06-15 15:30:58 -0400

    prebuild work

--- prebuild.sh
+++ prebuild.sh
@@ -4,12 +4,10 @@ echo "---- Populating sensitive /downloads folder ------------------------------
 #### GET ENVARS #################################################
 SHARED_DIR=$1
 
-if [ -f "$SHARED_DIR/config/envvars" ]; then
-  . $SHARED_DIR/config/envvars
-  printf "Found your local envvars file. Using it."
-
+if [ -f "config/envvars" ]; then
+  . config/envvars
+  printf "found your local envvars file. Using it."
 else
-  . $SHARED_DIR/config/envvars.default
   printf "Could not find envvars - remember to copy /config/envvars.* (e.g. envvars.public) to /config/envvars.  Aborting."
   exit 1
 fi
